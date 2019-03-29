2d0f3c53f6f690d31859759402f7de541bea9d9c
Alex <alex.donn@hotmail.com>
2019-03-29 16:56:36 +1000

    web

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask as f
 import json
+import os
 from passlib.hash import sha256_crypt
 from werkzeug.utils import secure_filename
 
@@ -7,27 +8,30 @@ app = f.Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "static/store"
 
 
+# <editor-fold desc="Authorisation">
 @app.route("/")
 def preauth():
-    return f.render_template("index.html")
+    return f.render_template("index.html", failed=False)
 
 
 @app.route("/auth", methods=["POST"])
 def auth():
-    return
+    if authcheck(f.request.form["user"], f.request.form["passw"]):
+        return f.render_template_string("Yes")
+    else:
+        return f.render_template("index.html", failed=True)
 
-
-def items():
-    return
 
 # user check
 def authcheck(username, password):
     with open('static/data/accounts.json') as r:
         data = json.load(r)
         for u in data['accounts']:
-            if sha256_crypt.verify(password, u['password']):
-                return True
+            if u['username'] == username:
+                if sha256_crypt.verify(password, u['password']):
+                    return True
     return False
+
 
 # Setup Account for Access
 def accountcreate(username, password):
@@ -45,6 +49,29 @@ def accountcreate(username, password):
         })
         json.dump(data, w, indent=4)
     return True
+
+
+# </editor-fold>
+
+
+# <editor-fold desc="Url_for">
+# Solves the issue where the browser loads cached static files instead of updated ones
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return f.url_for(endpoint, **values)
+
+
+# </editor-fold>
 
 
 app.run(debug=True)
